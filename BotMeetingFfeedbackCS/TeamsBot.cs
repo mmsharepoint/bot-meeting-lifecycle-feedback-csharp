532087ac4c1c701edcd7fb7d9ca8c334fed6424b
mmsharepoint <markus.moeller.ed@outlook.de>
2023-12-20 10:04:19 +0100

    1st commit

---
+++ BotMeetingFfeedbackCS/TeamsBot.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+
+namespace BotMeetingFfeedbackCS
+{
+    /// <summary>
+    /// An empty bot handler.
+    /// You can add your customization code here to extend your bot logic if needed.
+    /// </summary>
+    public class TeamsBot : TeamsActivityHandler
+    {
+        protected override async Task OnTeamsMeetingStartAsync(Microsoft.Bot.Schema.Teams.MeetingStartEventDetails meeting, Microsoft.Bot.Builder.ITurnContext<Microsoft.Bot.Schema.IEventActivity> turnContext, System.Threading.CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync("Meeting started");
+        }
+        
+        public override Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default) =>
+            Task.CompletedTask;
+    }
+}
